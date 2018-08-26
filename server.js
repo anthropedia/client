673506848e62c853a25b8d71d076b427f0c4b277
vinyll <vincent.agnano@scopyleft.fr>
2018-08-26 09:37:59 +0200

    added internal server and setup natural routes, no hashbang

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(serveStatic(__dirname))
+
+app.all('*', (_req, res) => res.sendFile(`${__dirname}/index.html`))
+
+app.listen(port, () => console.info(`App is ready on port ${port}`))
